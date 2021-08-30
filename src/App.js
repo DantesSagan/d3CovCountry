41a3824b4c7a612549e3fc804299478a4e7c6287
lexsander ikhaylov <mihaleksval@gmail.com>
2021-08-31 02:21:34 +0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import * as ROUTES from '../src/constants/routes';
+
+import TotalCases from './components/RusCovid/TotalCases/TotalCases';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg className='App'>
+      <TotalCases />
+    </svg>
   );
 }
-
-export default App;
